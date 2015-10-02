b31dbc6c7fc674e68d1b811cb705dd5c2ca07806
aa1143 <814185384@qq.com>
2015-10-02 20:23:32 +0800

    add exercise_1.4

---
+++ ch01/exercise_1.4.cpp
@@ -0,0 +1,15 @@
+//
+// Created by zhang zhixiang on 15/10/2.
+//
+
+#include <iostream>
+
+int  main(){
+    int v1;
+    int v2;
+    std::cout << "Please enter two numbers :" <<std::endl;
+    std::cin >> v1 >> v2 ;
+    std::cout << "the product of is " << v1 * v2 <<std::endl;
+
+    return 0;
+}
