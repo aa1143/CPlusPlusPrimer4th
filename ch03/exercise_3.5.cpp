522157a26a29cda0fbd4b7f9d8d35a10515537da
aa1143 <814185384@qq.com>
2015-10-04 01:17:55 +0800

    add ch03

---
+++ ch03/exercise_3.5.cpp
@@ -0,0 +1,22 @@
+//
+// Created by zhang zhixiang on 15/10/4.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    //string line;
+    //while(getline(cin,line)) {
+    //    cout << line <<endl;
+   //s }
+   // string word;
+    //while(cin >> word) {
+     //   cout << word <<endl;
+   // }
+    string tmp;
+    cin >> tmp;
+    cout << tmp <<endl;
+    return 0;
+}
