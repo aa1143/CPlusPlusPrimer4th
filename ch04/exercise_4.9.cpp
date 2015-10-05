43f07880f803e2b648bc9593a8ed3a1a4085b2c1
aa1143 <814185384@qq.com>
2015-10-05 20:55:40 +0800

    add ch04

---
+++ ch04/exercise_4.9.cpp
@@ -0,0 +1,17 @@
+//
+// Created by zhang zhixiang on 15/10/5.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int size =10;
+    int array[size];
+
+    for(size_t i = 0; i < size; ++i) {
+        array[i] =  i;
+    }
+    return 0;
+}
